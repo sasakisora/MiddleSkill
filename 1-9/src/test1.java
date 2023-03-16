4785edec4a96728ce4c02839a745cb70a4d58549
sasakisora <109976240+sasakisora@users.noreply.github.com>
2023-03-17 01:38:26 +0900

    3/17 佐々木宇宙　指摘取り込み

---
+++ 1-9/src/test1.java
@@ -0,0 +1,18 @@
+public class test1 {
+        public static void main(String[] args) {
+                
+//                1.数字の5と9の変数を定義し、足した数字を出力しなさい。
+        	int sasaki = 5;
+        	int murano = 9;
+        	
+        	System.out.println(sasaki+murano);
+                
+//                2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
+        	long kaneko = 1111111111;
+        	System.out.println(kaneko*2);
+                
+//                3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。        
+        	boolean b = true;
+        	System.out.println("b=" + b);
+        }
+}
